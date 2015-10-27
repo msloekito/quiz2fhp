6f5e90819903870efd03d5da3dabd622cbf19685
msloekito <msloekito@gmail.com>
2015-10-27 18:46:04 +0000

    added simple form

--- app/controllers/gears_controller.rb
+++ app/controllers/gears_controller.rb
@@ -2,4 +2,19 @@ class GearsController < ApplicationController
     def index
         @gears = Gear.all
     end
+    
+    def new
+        @gears = Gear.new
+    end
+    
+    def create
+        Gear.create(gear_params)
+        redirect_to root_path
+    end
+    
+    private
+    
+    def gear_params
+        params.require(:gear).permit(:name, :description, :owner)
+    end
 end
